f6649403417a448dfcaf290a9f63e0417defea79
Bence <szilagyibencee@gmail.com>
2024-05-28 16:58:54 +0200

    event manager added

---
+++ lib/event_service.dart
@@ -0,0 +1,92 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:univerx/models/eventModel.dart';
+
+class EventService {
+  final String url;
+  final now = DateTime(2024, 4, 30, 11, 30);
+  EventService(this.url);
+
+  Future<List<Event>> fetchEvents() async {
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        final icsData = response.body;
+        final events = parseICS(icsData);
+        events.sort((a, b) => a.start.compareTo(b.start)); // Sort events by start time
+        return events;
+      } else {
+        throw Exception('Failed to load events');
+      }
+    } catch (e) {
+      print('Error fetching events: $e');
+      rethrow;
+    }
+  }
+
+  List<Event> parseICS(String icsData) {
+    try {
+      final events = <Event>[];
+      final eventStrings = icsData.split('BEGIN:VEVENT');
+      for (var eventString in eventStrings.skip(1)) {
+        events.add(Event.fromICS(eventString));
+      }
+      return events;
+    } catch (e) {
+      print('Error parsing ICS data: $e');
+      rethrow;
+    }
+  }
+
+  Future<Event?> getCurrentEvent() async {
+    final events = await fetchEvents();
+    // final now = DateTime.now(); //-------------------------------REMOVE COMMENT TO ENABLE LIVE TIME-----------------------------
+    for (final event in events) {
+      if (event.start.isBefore(now) && event.end.isAfter(now)) {
+        return event;
+      }
+    }
+    return null; // No event currently happening
+  }
+
+  Future<Event?> getUpcomingEvent() async {
+    final events = await fetchEvents();
+    // final now = DateTime.now();
+    for (final event in events) {
+      if (event.start.isAfter(now)) {
+        return event;
+      }
+    }
+    return null; // No upcoming events
+  }
+  String formatTimeLeft(int minutes) {
+    final hours = minutes ~/ 60;
+    final mins = minutes % 60;
+    return '${hours}h ${mins}m';
+  }
+
+  Future<String?> timeLeftForCurrentEvent() async {
+    final currentEvent = await getCurrentEvent();
+    if (currentEvent != null) {
+      // final now = DateTime.now(); //-------------------------------REMOVE COMMENT TO ENABLE LIVE TIME-----------------------------
+
+      final timeLeft = currentEvent.end.difference(now).inMinutes;
+      return formatTimeLeft(timeLeft);
+    }
+    return null; // No event currently happening
+  }
+
+  Future<double?> percentagePassedOfCurrentEvent() async {
+    final currentEvent = await getCurrentEvent();
+    if (currentEvent != null) {
+      //final now = DateTime.now(); //-------------------------------REMOVE COMMENT TO ENABLE LIVE TIME-----------------------------
+
+      final duration = currentEvent.end.difference(currentEvent.start).inMinutes;
+      final timePassed = now.difference(currentEvent.start).inMinutes;
+      final percentagePassed = timePassed / duration;
+      return percentagePassed;
+    }
+    return null; // No event currently happening
+  }
+ 
+}
