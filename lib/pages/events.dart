7755e6734127aecf7220fc4f7c06e26eee31cd27
Bence <szilagyibencee@gmail.com>
2024-05-31 00:03:34 +0200

    add .icslink button added

--- lib/pages/events.dart
+++ lib/pages/events.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:univerx/pages/home.dart';
 import 'package:univerx/database_helper.dart'; // Assuming you use the same database helper for assignments
 import 'package:univerx/models/eventModel.dart'; // Assuming you have a model for assignments
 import 'package:univerx/event_service.dart'; // Assuming you have a model for assignments
+import 'package:univerx/events/fetchAndUpdateEvents.dart'; // Assuming you have a model for assignments
+
 class Events extends StatefulWidget {
   const Events({Key? key}) : super(key: key);
 
@@ -26,6 +29,56 @@ class _AssignmentsState extends State<Events> {
     });
   }
 
+  void _showIcsLinkInputDialog(BuildContext context) async{
+    final TextEditingController icsLinkController = TextEditingController();
+
+    final result = await DatabaseHelper.instance.getCalendarICS();
+    if (result != null) {
+      icsLinkController.text = result.toString();
+    }
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Enter .ics Link'),
+          content: TextField(
+            controller: icsLinkController,
+            decoration: InputDecoration(
+              hintText: 'https://sample.com/a.ics',
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                // ezt itt szebben kene EEEEEEEEEEEEEEEEEEEEEDTSADKSHFDAHSFLSAJFLKASJFLKSAJFKLS
+                String icsLink = icsLinkController.text;
+                if (icsLink == '') {
+                  DatabaseHelper.instance.updateCalendarICS(icsLink);
+                  DatabaseHelper.instance.clearAllEvents();
+                }
+                else if (result == null) {
+                  DatabaseHelper.instance.saveCalendarICS(icsLink);
+                  await fetchAndUpdateEventsFromIcs(icsLink);
+                } else{
+                  DatabaseHelper.instance.updateCalendarICS(icsLink);
+                  await fetchAndUpdateEventsFromIcs(icsLink);
+                }
+                Navigator.of(context).pop(); // Close the dialog
+                _loadEvents();
+              },
+              child: Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,14 +106,25 @@ class _AssignmentsState extends State<Events> {
           actions: [
             IconButton(
               onPressed: () {
+                _showIcsLinkInputDialog(context);
+              },
+              icon: const CircleAvatar(
+                backgroundColor: Colors.blue, // Change the color as needed
+                child: Icon(
+                  Icons.add_link, color: Colors.white, size: 20.0,
+                )
+              ),
+            ),
+            IconButton(
+              onPressed: () {
                 // to second page
                 //Navigator.pushNamed(context, '/second');
               },
               icon: const CircleAvatar(
-                backgroundColor: Colors.blue, // Change the color as needed
+                backgroundColor: Color.fromARGB(255, 20, 21, 27), // Change the color as needed
                 child: Text(
                   "D", // Replace with your letter
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(color: Colors.white,fontFamily: "sfpro"),
                 ),
               ),
             )
