a4ddaa2e1ad2d0e5c018b4fa00af754b37a17459
Bence <szilagyibencee@gmail.com>
2024-05-31 17:17:45 +0200

    appBar widget

---
+++ lib/features/common/widgets/default_app_bar.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:univerx/features/home/presentation/pages/homePage.dart';
+
+
+
+
+
+class DefaultAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final bool showBackButton;
+
+
+  // ---------------------Requirement Title--------------------------
+  DefaultAppBar({
+    required this.title,
+    this.showBackButton = false,
+  });
+
+  // ---------------------AppBar--------------------------
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            title,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 16.0,
+              fontFamily: "sfpro",
+            ),
+          ),
+        ],
+      ),
+      
+      leading: showBackButton
+        ? IconButton(
+            icon: Icon(Icons.arrow_back),
+            color: Color.fromARGB(255, 255, 255, 255),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          )
+        : null,
+
+      backgroundColor: Colors.black,
+      elevation: 0,
+      actions: [
+        IconButton(
+          onPressed: (){}, // nothing
+          icon: const CircleAvatar(
+            backgroundColor: Color.fromARGB(255, 20, 21, 27),
+            child: Text(
+              "D",
+              style: TextStyle(color: Colors.white, fontFamily: "sfpro"),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
