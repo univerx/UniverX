877bd18e689ee9fa9da0381ed47a140596a94f46
Bence <szilagyibencee@gmail.com>
2024-06-04 19:21:29 +0200

    new custom calendar table

---
+++ lib/features/calendar/presentation/widgets/customCalendar.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:univerx/features/calendar/data/model/calendarModel.dart';
+
+class CustomCalendar extends StatelessWidget {
+  final DateTime focusedDay;
+  final DateTime? selectedDay;
+  final ValueNotifier<List<EventModel>> selectedEvents;
+  final Function(DateTime selectedDay, DateTime focusedDay) onDaySelected;
+  final Function(DateTime focusedDay) onPageChanged;
+  final List<EventModel> Function(DateTime day) eventLoader;
+
+  CustomCalendar({
+    required this.focusedDay,
+    required this.selectedDay,
+    required this.selectedEvents,
+    required this.onDaySelected,
+    required this.onPageChanged,
+    required this.eventLoader,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar<EventModel>(
+      firstDay: DateTime.utc(2020, 1, 1),
+      lastDay: DateTime.utc(2030, 12, 31),
+      focusedDay: focusedDay,
+      calendarFormat: CalendarFormat.month,
+      selectedDayPredicate: (day) => isSameDay(selectedDay, day),
+      startingDayOfWeek: StartingDayOfWeek.monday,
+      onDaySelected: onDaySelected,
+      onPageChanged: onPageChanged,
+      eventLoader: eventLoader,
+      availableCalendarFormats: const {CalendarFormat.month: 'Month'},
+      headerStyle: const HeaderStyle(
+        formatButtonVisible: false,
+        titleCentered: true,
+        leftChevronIcon: Icon(Icons.chevron_left, color: Colors.white),
+        rightChevronIcon: Icon(Icons.chevron_right, color: Colors.white),
+        titleTextStyle: TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold),
+      ),
+      calendarStyle: const CalendarStyle(
+        defaultTextStyle: TextStyle(color: Colors.white),
+        weekendTextStyle: TextStyle(color: Color.fromARGB(255, 249, 249, 249)),
+        todayDecoration: BoxDecoration(
+          color: Color.fromARGB(255, 35, 37, 48),
+          shape: BoxShape.circle,
+        ),
+        selectedDecoration: BoxDecoration(
+          color: Color.fromARGB(255, 163, 172, 222),
+          shape: BoxShape.circle,
+        ),
+      ),
+      daysOfWeekStyle: const DaysOfWeekStyle(
+        weekdayStyle: TextStyle(color: Colors.white),
+        weekendStyle: TextStyle(color: Color.fromARGB(255, 255, 0, 0)),
+      ),
+    );
+  }
+}
