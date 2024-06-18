f06856bb8b7e653fb76dfa2bdbfae6d54e6df880
Bence <szilagyibencee@gmail.com>
2024-06-18 23:32:37 +0200

    login small design change

--- lib/features/neptun_login/presentation/pages/login.dart
+++ lib/features/neptun_login/presentation/pages/login.dart
@@ -7,6 +7,7 @@ import 'dart:convert';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:univerx/features/home/presentation/pages/homePage.dart';
 import 'package:univerx/features/neptun_login/data/neptunApi.dart';
+import 'package:univerx/features/common/widgets/box_3d.dart'; // Import the custom decorations
 
 class LoginPage extends StatefulWidget {
   @override
@@ -25,8 +26,6 @@ class _LoginPageState extends State<LoginPage> {
     super.initState();
     _loadUniversities();
   }
-
-
 
   Future<void> _loadUniversities() async {
     String data = await rootBundle.loadString('lib/features/neptun_login/data/Institutes.json');
@@ -58,119 +57,102 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: const Color.fromARGB(255, 20, 18, 32),
       body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Colors.transparent,
-              Color.fromARGB(167, 84, 76, 128).withOpacity(0.2),
-              Color.fromARGB(156, 12, 6, 36),
-            ],
-            stops: [0.3, 0.6, 1.0],
-            begin: Alignment.bottomRight,
-            end: Alignment.topLeft,
-          ),
-        ),
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.transparent,
-                Color.fromARGB(167, 82, 74, 125).withOpacity(0.2),
-                Color.fromARGB(156, 12, 6, 36),
-              ],
-              stops: [0.3, 0.6, 1.0],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(32.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Text(
-                    'Login',
-                    style: TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      letterSpacing: 3.0,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    letterSpacing: 3.0,
+                  ),
+                ),
+                const SizedBox(height: 24.0), // Space between the title and the first input field
+                DropdownButton<String>(
+                  value: selectedUniversity,
+                  dropdownColor: Colors.grey[800],
+                  hint: Text(
+                    'Select University',
+                    style: TextStyle(color: Colors.grey[500]),
+                  ),
+                  isExpanded: true,
+                  items: universities.map<DropdownMenuItem<String>>((university) {
+                    return DropdownMenuItem<String>(
+                      value: university['Name'],
+                      child: Text(
+                        university['Name'],
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      selectedUniversity = value;
+                      selectedUniversityUrl = universities.firstWhere(
+                        (uni) => uni['Name'] == value,
+                        orElse: () => {'Url': null}
+                      )['Url'];
+                    });
+                  },
+                ),
+                const SizedBox(height: 24.0),
+                TextField(
+                  controller: neptunController,
+                  decoration: InputDecoration(
+                    hintText: 'neptun code',
+                    hintStyle: const TextStyle(
+                      color: Colors.grey, // Placeholder text color
+                      fontSize: 16.0, // Placeholder text size
+                      letterSpacing: 1.5,
+                    ),
+                    filled: true,
+                    fillColor: Colors.transparent,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
                     ),
                   ),
-                  const SizedBox(height: 24.0), // Space between the title and the first input field
-                  DropdownButton<String>(
-                    value: selectedUniversity,
-                    dropdownColor: Colors.grey[800],
-                    hint: Text(
-                      'Select University',
-                      style: TextStyle(color: Colors.grey[500]),
+                ),
+                const SizedBox(height: 24.0),
+                TextField(
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                    hintText: 'password',
+                    hintStyle: const TextStyle(
+                      color: Colors.grey, // Placeholder text color
+                      fontSize: 16.0, // Placeholder text size
+                      letterSpacing: 1.5,
                     ),
-                    isExpanded: true,
-                    items: universities.map<DropdownMenuItem<String>>((university) {
-                      return DropdownMenuItem<String>(
-                        value: university['Name'],
-                        child: Text(
-                          university['Name'],
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      );
-                    }).toList(),
-                    onChanged: (value) {
-                      setState(() {
-                        selectedUniversity = value;
-                        selectedUniversityUrl = universities.firstWhere(
-                          (uni) => uni['Name'] == value,
-                          orElse: () => {'Url': null}
-                        )['Url'];
-                      });
-                    },
-                  ),
-                  const SizedBox(height: 24.0),
-                  TextField(
-                    controller: neptunController,
-                    decoration: InputDecoration(
-                      hintText: 'neptun code',
-                      hintStyle: const TextStyle(
-                        color: Colors.grey, // Placeholder text color
-                        fontSize: 16.0, // Placeholder text size
-                        letterSpacing: 1.5,
-                      ),
-                      filled: true,
-                      fillColor: Colors.transparent,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12.0),
-                      ),
+                    filled: true,
+                    fillColor: Colors.transparent,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
                     ),
                   ),
-                  const SizedBox(height: 24.0),
-                  TextField(
-                    controller: passwordController,
-                    decoration: InputDecoration(
-                      hintText: 'password',
-                      hintStyle: const TextStyle(
-                        color: Colors.grey, // Placeholder text color
-                        fontSize: 16.0, // Placeholder text size
-                        letterSpacing: 1.5,
-                      ),
-                      filled: true,
-                      fillColor: Colors.transparent,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12.0),
-                      ),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 30.0),
+                Container(
+                  width: 175,
+                  decoration: Box3D(),
+                   // Apply the custom box decoration
+                  child: Container(
+                    margin: EdgeInsets.all(1), // Adjust this value to change the border width
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Color.fromARGB(255, 38, 51, 70),
                     ),
-                    obscureText: true,
-                  ),
-                  const SizedBox(height: 48.0),
-                  SizedBox(
-                    width: double.infinity, // To make the button full width
                     child: ElevatedButton(
                       onPressed: () async {
                         final neptunCode = neptunController.text;
                         final password = passwordController.text;
-
+                    
                         if (selectedUniversityUrl != null && neptunCode.isNotEmpty && password.isNotEmpty) {
                           if(await checkLoginDetails(selectedUniversityUrl!, neptunCode, password) == true){
                             await DatabaseHelper.instance.saveNeptunLogin(
@@ -185,7 +167,7 @@ class _LoginPageState extends State<LoginPage> {
                               context,
                               MaterialPageRoute(builder: (context) => const Home()), // Ensure Home is correctly imported
                             );
-
+                    
                           }
                           else {
                             print('Wrong details');
@@ -194,15 +176,15 @@ class _LoginPageState extends State<LoginPage> {
                         } else {
                           print('Missing details');
                           _showWrongPasswordPopup();
-
                         }
                       },
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue,
+                        backgroundColor: Colors.transparent,
                         padding: const EdgeInsets.symmetric(vertical: 16.0),
                         shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
+                          borderRadius: BorderRadius.circular(20.0),
                         ),
+                        elevation: 0, // Remove button shadow
                       ),
                       child: const Text(
                         'Login',
@@ -213,8 +195,8 @@ class _LoginPageState extends State<LoginPage> {
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
