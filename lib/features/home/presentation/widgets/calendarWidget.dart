48a7abca25386a06fa1d1c4321b0cc48c65eac3f
Bence <szilagyibencee@gmail.com>
2024-06-18 23:32:16 +0200

    ksdmaksdm

--- lib/features/home/presentation/widgets/calendarWidget.dart
+++ lib/features/home/presentation/widgets/calendarWidget.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:univerx/features/calendar/data/model/calendarModel.dart';
-
 import 'package:univerx/event_service.dart';
-
+import 'package:univerx/features/common/widgets/box_3d.dart';
 
 class CalendarWidget extends StatelessWidget {
   final Future<EventModel?> currentEvent;
@@ -19,6 +18,7 @@ class CalendarWidget extends StatelessWidget {
     required this.homeContext,
   });
   final eventService = EventService('');
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -31,49 +31,22 @@ class CalendarWidget extends StatelessWidget {
             child: GestureDetector(
               onTap: () => Navigator.pushNamed(homeContext, '/events'),
               child: Container(
-                height: 140,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(40),
-                  color: Color.fromARGB(255, 20, 21, 27),
-                ),
-                padding: EdgeInsets.all(15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        FutureBuilder<EventModel?>(
-                          future: currentEvent,
-                          builder: (context, snapshot) {
-                            if (snapshot.hasError) {
-                              return Center(child: Text('Error: ${snapshot.error}'));
-                            } else if (snapshot.hasData) {
-                              final event = snapshot.data;
-                              if (event != null) {
-                                return Text(
-                                  EventModel.getFormattedTitle(event.summary),
-                                  style: const TextStyle(
-                                    fontSize: 20.0,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                );
-                              } else {
-                                return Text('No event currently happening');
-                              }
-                            } else {
-                              return Text('No event currently happening');
-                            }
-                          },
-                        ),
-                        const SizedBox(width: 5),
-                        Container(
-                          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                          decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 35, 37, 48),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: FutureBuilder<EventModel?>(
+                margin: EdgeInsets.only(left: 10, right: 10),
+                height: 113,
+                decoration: Box3D(),
+                child: Container(
+                  margin: EdgeInsets.all(1), // Adjust this value to change the border width
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Color.fromARGB(255, 38, 51, 70),
+                  ),
+                  padding: EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          FutureBuilder<EventModel?>(
                             future: currentEvent,
                             builder: (context, snapshot) {
                               if (snapshot.hasError) {
@@ -82,58 +55,58 @@ class CalendarWidget extends StatelessWidget {
                                 final event = snapshot.data;
                                 if (event != null) {
                                   return Text(
-                                    '${event.location.split(" ").first}',
+                                    EventModel.getFormattedTitle(event.summary),
                                     style: const TextStyle(
-                                      color: Color.fromARGB(255, 163, 172, 222),
+                                      fontSize: 20.0,
+                                      color: Colors.white,
                                       fontWeight: FontWeight.bold,
                                     ),
                                   );
                                 } else {
-                                  return Text('-');
+                                  return Text('No event currently happening');
                                 }
                               } else {
-                                return Text('-');
+                                return Text('No event currently happening');
                               }
                             },
                           ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 5),
-                    Row(
-                      children: [
-                        FutureBuilder<EventModel?>(
-                          future: upcomingEvent,
-                          builder: (context, snapshot) {
-                            if (snapshot.hasError) {
-                              return Center(child: Text('Error: ${snapshot.error}'));
-                            } else if (snapshot.hasData) {
-                              final event = snapshot.data;
-                              if (event != null) {
-                                return Text(
-                                  '→ ${EventModel.getFormattedTitle(event.summary)}',
-                                  style: TextStyle(
-                                    fontSize: 14.0,
-                                    color: Colors.white,
-                                    fontFamily: "sfpro",
-                                  ),
-                                );
-                              } else {
-                                return Text('No upcoming event.');
-                              }
-                            } else {
-                              return Text('No upcoming event.');
-                            }
-                          },
-                        ),
-                        SizedBox(width: 5),
-                        Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 3),
-                          decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 35, 37, 48),
-                            borderRadius: BorderRadius.circular(10),
+                          const SizedBox(width: 5),
+                          Container(
+                            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                            decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 20, 18, 32),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: FutureBuilder<EventModel?>(
+                              future: currentEvent,
+                              builder: (context, snapshot) {
+                                if (snapshot.hasError) {
+                                  return Center(child: Text('Error: ${snapshot.error}'));
+                                } else if (snapshot.hasData) {
+                                  final event = snapshot.data;
+                                  if (event != null) {
+                                    return Text(
+                                      '${event.location.split(" ").first}',
+                                      style: const TextStyle(
+                                        color: Color.fromARGB(255, 255, 255, 255),
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    );
+                                  } else {
+                                    return Text('-');
+                                  }
+                                } else {
+                                  return Text('-');
+                                }
+                              },
+                            ),
                           ),
-                          child: FutureBuilder<EventModel?>(
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          FutureBuilder<EventModel?>(
                             future: upcomingEvent,
                             builder: (context, snapshot) {
                               if (snapshot.hasError) {
@@ -142,76 +115,109 @@ class CalendarWidget extends StatelessWidget {
                                 final event = snapshot.data;
                                 if (event != null) {
                                   return Text(
-                                    '${event.location.split(" ").first}',
-                                    style: const TextStyle(
-                                      color: Color.fromARGB(255, 163, 172, 222),
-                                      fontWeight: FontWeight.bold,
+                                    '→ ${EventModel.getFormattedTitle(event.summary)}',
+                                    style: TextStyle(
+                                      fontSize: 14.0,
+                                      color: Colors.white,
+                                      fontFamily: "sfpro",
                                     ),
                                   );
                                 } else {
-                                  return Text('-');
+                                  return Text('No upcoming event.');
                                 }
                               } else {
-                                return Text('-');
+                                return Text('No upcoming event.');
                               }
                             },
                           ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 12),
-                    FutureBuilder<String?>(
-                      future: timeLeftForEvent,
-                      builder: (context, snapshot) {
-                        if (snapshot.hasError) {
-                          return Center(child: Text('Error: ${snapshot.error}'));
-                        } else if (snapshot.hasData) {
-                          final timeLeft = snapshot.data;
-                          if (timeLeft != null) {
-                            return Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Text(
-                                  "Time left: ${timeLeft}",
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 10,
+                          SizedBox(width: 5),
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                            decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 20, 18, 32),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: FutureBuilder<EventModel?>(
+                              future: upcomingEvent,
+                              builder: (context, snapshot) {
+                                if (snapshot.hasError) {
+                                  return Center(child: Text('Error: ${snapshot.error}'));
+                                } else if (snapshot.hasData) {
+                                  final event = snapshot.data;
+                                  if (event != null) {
+                                    return Text(
+                                      '${event.location.split(" ").first}',
+                                      style: const TextStyle(
+                                        color: Color.fromARGB(255, 255, 255, 255),
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 10,
+                                      ),
+                                    );
+                                  } else {
+                                    return Text('-');
+                                  }
+                                } else {
+                                  return Text('-');
+                                }
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                      //const SizedBox(height: 0),
+                      FutureBuilder<String?>(
+                        future: timeLeftForEvent,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasError) {
+                            return Center(child: Text('Error: ${snapshot.error}'));
+                          } else if (snapshot.hasData) {
+                            final timeLeft = snapshot.data;
+                            if (timeLeft != null) {
+                              return Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    "Time left: ${timeLeft}",
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 10,
+                                    ),
                                   ),
-                                ),
-                              ],
-                            );
+                                ],
+                              );
+                            } else {
+                              return const Text('');
+                            }
                           } else {
                             return const Text('');
                           }
-                        } else {
-                          return const Text('');
-                        }
-                      },
-                    ),
-                    const SizedBox(height: 5),
-                    FutureBuilder<double?>(
-                      future: percentagePassedForEvent,
-                      builder: (context, snapshot) {
-                        if (snapshot.hasError) {
-                          return Center(child: Text('Error: ${snapshot.error}'));
-                        } else {
-                          var percentagePassed = snapshot.data;
-                          if (percentagePassed == null) {
-                            percentagePassed = 0;
+                        },
+                      ),
+                      const SizedBox(height: 5),
+                      FutureBuilder<double?>(
+                        future: percentagePassedForEvent,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasError) {
+                            return Center(child: Text('Error: ${snapshot.error}'));
+                          } else {
+                            var percentagePassed = snapshot.data;
+                            if (percentagePassed == null) {
+                              percentagePassed = 0;
+                            }
+                            return SizedBox(
+                              height: 10,
+                              child: LinearProgressIndicator(
+                                backgroundColor: const Color.fromARGB(255, 20, 18, 32),
+                                valueColor: const AlwaysStoppedAnimation<Color>(Color.fromARGB(255, 255, 255, 255)),
+                                borderRadius: BorderRadius.circular(20),
+                                value: percentagePassed,
+                              ),
+                            );
                           }
-                          return SizedBox(
-                            height: 10,
-                            child: LinearProgressIndicator(
-                              backgroundColor: Color.fromARGB(255, 43, 44, 49),
-                              valueColor: const AlwaysStoppedAnimation<Color>(Color.fromARGB(255, 255, 255, 255)),
-                              borderRadius: BorderRadius.circular(20),
-                              value: percentagePassed,
-                            ),
-                          );
-                        }
-                      },
-                    ),
-                  ],
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
