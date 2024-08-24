b6101d87c662f470a6ea22b6bfb62f56fbe307c7
31b4 <szilagyibencee@gmail.com>
2024-08-24 22:25:54 +0200

    old login archive

---
+++ lib/features/neptun_login/login_old.dart
@@ -0,0 +1,249 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:univerx/features/common/widgets/default_app_bar.dart';
+import 'package:univerx/features/common/widgets/profile_menu.dart';
+import 'package:univerx/database/database_helper.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:univerx/features/home/homePage.dart';
+import 'package:univerx/services/neptun_API_fetching.dart';
+import 'package:univerx/features/common/widgets/box_3d.dart'; // Import the custom decorations
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController neptunController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  List<dynamic> universities = [];
+  String? selectedUniversity = "Pécsi Tudományegyetem";
+  String? selectedUniversityUrl = "https://neptun-web4.tr.pte.hu/hallgato/MobileService.svc";
+  bool loginEnabled = true;
+  bool loggingIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUniversities();
+  }
+
+  Future<void> _loadUniversities() async {
+    String data = await rootBundle.loadString('lib/features/neptun_login/data/Institutes.json');
+    setState(() {
+      universities = json.decode(data);
+    });
+  }
+
+  void _showWrongPasswordPopup() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Error'),
+          content: Text('Wrong details!'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 20, 18, 32),
+      body: Stack(
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      letterSpacing: 3.0,
+                    ),
+                  ),
+                  const SizedBox(height: 24.0), // Space between the title and the first input field
+                  DropdownButton<String>(
+                    value: selectedUniversity,
+                    dropdownColor: Colors.grey[800],
+                    hint: Text(
+                      'Select University',
+                      style: TextStyle(color: Colors.grey[500]),
+                    ),
+                    isExpanded: true,
+                    items: universities.map<DropdownMenuItem<String>>((university) {
+                      return DropdownMenuItem<String>(
+                        value: university['Name'],
+                        child: Text(
+                          university['Name'],
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: loggingIn
+                        ? null
+                        : (value) {
+                            setState(() {
+                              selectedUniversity = value;
+                              selectedUniversityUrl = universities.firstWhere(
+                                (uni) => uni['Name'] == value,
+                                orElse: () => {'Url': null},
+                              )['Url'];
+                            });
+                          },
+                  ),
+                  const SizedBox(height: 24.0),
+                  TextField(
+                    controller: neptunController,
+                    decoration: InputDecoration(
+                      hintText: 'neptun code',
+                      hintStyle: const TextStyle(
+                        color: Colors.grey, // Placeholder text color
+                        fontSize: 16.0, // Placeholder text size
+                        letterSpacing: 1.5,
+                      ),
+                      filled: true,
+                      fillColor: Colors.transparent,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                    ),
+                    enabled: !loggingIn,
+                  ),
+                  const SizedBox(height: 24.0),
+                  TextField(
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      hintText: 'password',
+                      hintStyle: const TextStyle(
+                        color: Colors.grey, // Placeholder text color
+                        fontSize: 16.0, // Placeholder text size
+                        letterSpacing: 1.5,
+                      ),
+                      filled: true,
+                      fillColor: Colors.transparent,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                    ),
+                    obscureText: true,
+                    enabled: !loggingIn,
+                  ),
+                  const SizedBox(height: 30.0),
+                  Container(
+                    width: 175,
+                    decoration: Box3D(),
+                    // Apply the custom box decoration
+                    child: Container(
+                      margin: EdgeInsets.all(1), // Adjust this value to change the border width
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color.fromARGB(255, 38, 51, 70),
+                      ),
+                      child: ElevatedButton(
+                        onPressed: loginEnabled
+                            ? () async {
+                                setState(() {
+                                  loginEnabled = false;
+                                  loggingIn = true;
+                                });
+
+                                final neptunCode = neptunController.text;
+                                final password = passwordController.text;
+
+                                if (selectedUniversityUrl != null &&
+                                    neptunCode.isNotEmpty &&
+                                    password.isNotEmpty) {
+                                  if (await checkLoginDetails(selectedUniversityUrl!, neptunCode, password) == true) {
+                                    await DatabaseHelper.instance.saveNeptunLogin(
+                                      selectedUniversity!,
+                                      selectedUniversityUrl!,
+                                      neptunCode,
+                                      password,
+                                    );
+                                    // Navigate to the home page
+                                    Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(builder: (context) => const Home()), // Ensure Home is correctly imported
+                                    );
+                                  } else {
+                                    print('Wrong details');
+                                    _showWrongPasswordPopup();
+                                  }
+                                } else {
+                                  print('Missing details');
+                                  _showWrongPasswordPopup();
+                                }
+
+                                setState(() {
+                                  loginEnabled = true;
+                                  loggingIn = false;
+                                });
+                              }
+                            : null,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                          padding: const EdgeInsets.symmetric(vertical: 16.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          elevation: 0, // Remove button shadow
+                        ),
+                        child: loggingIn
+                            ? const SizedBox(
+                                width: 22,
+                                height: 22,
+                                child: CircularProgressIndicator(
+                                  strokeWidth: 3,
+                                  color: Colors.white,
+                                ),
+                              )
+                            : const Text(
+                                'Login',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Positioned(
+            bottom: 16.0,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: Text(
+                'v0.4.2 beta',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 12.0,
+                  fontFamily: "sfpro",
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
