3cdea3ce8ad59e15e36a1d201ef9ae00c92928f5
31b4 <szilagyibencee@gmail.com>
2024-08-24 22:24:46 +0200

    new db

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -18,7 +18,7 @@ class DatabaseHelper {
   Future<Database> get database async {
     if (_database != null) return _database!;
 
-    _database = await _initDB('test_8.db');
+    _database = await _initDB('test_11.db');
     
     return _database!;
   }
