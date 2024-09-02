24aa4de7b892ba9f6c12ea3c069744eef668b072
31b4 <szilagyibencee@gmail.com>
2024-09-02 12:56:04 +0200

    live time sync

--- lib/database/appdata.dart
+++ lib/database/appdata.dart
@@ -1,1 +1,24 @@
+import 'package:univerx/models/class.dart';
+
 const String version_number = 'v1.0.1';
+
+// create class for time with setter and getter with Datetime
+class CurrentTime{
+  DateTime _time = DateTime.now();
+  //DateTime _time = DateTime(2024,09,02,11,40);
+
+  DateTime get_time(){
+    updateTime();
+    return _time;
+  }
+
+
+  // update time with datetime.now
+  void updateTime(){
+    _time = DateTime.now();
+    //_time = DateTime(2024,09,02,11,40);
+  }
+}
+
+
+//final DateTime set_time = DateTime.now();
