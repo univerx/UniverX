012f00f51c315327e4b149af1aa7b8a0b4507fa3
31b4 <szilagyibencee@gmail.com>
2024-09-03 19:22:53 +0200

    not done

--- lib/services/titleGenerator.dart
+++ lib/services/titleGenerator.dart
@@ -1,19 +1,62 @@
+import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:univerx/database/appdata.dart';
+import 'package:univerx/database/database_helper.dart';
+import 'package:univerx/models/class.dart';
 
 String welcomeGenerator() {
-  String res = "";
-  DateTime now = DateTime.now();
-  // if weekend
+  CurrentTime time = CurrentTime();
+  final now = time.get_time();
+  // final now = DateTime(2024,09,03,03,00);
+
+  var ttne = Duration(hours: 9); // needs to be fixed
+
   if (now.weekday == 6 || now.weekday == 7) {
-    res = "Kellemes hétvégét!";
-  } else if (now.hour < 12) {
-    res = "Jó reggelt!";
-  } else if (now.hour < 18) {
-    res = "Kellemes délutánt!";
+    return "Kellemes hétvégét!";
+  } else if (now.hour < 12 && now.hour > 5) {
+    return "Jó reggelt!";
+  } else if (now.hour < 18 && now.hour >= 12) {
+    return "Kellemes délutánt!";
   } else {
-    res = "Jó estét!";
+    if (ttne != null) {
+      if (ttne.inHours >= 9) {
+        return "Jó estét!";
+      } else if (ttne.inHours >= 8) {
+        return "Jó éjszakát!";
+      } else if (ttne.inHours >= 7) {
+        return "Ideje aludni!";
+      } else if (ttne.inHours >= 6) {
+        return "Na tesoooo 😴";
+      } else if (ttne.inHours >= 5) {
+        return "Teso neeee!";
+      } else {
+        return "💀💀💀";
+      }
+    } else {
+      return "Jó estét!";
+    }
   }
-
-
-  return res;
 }
+
+
+
+
+
+
+  Future<DateTime?> getUpcomingEvent() async {
+  CurrentTime time = CurrentTime();
+  DateTime now = time.get_time();
+  var events = await DatabaseHelper.instance.getClasses();
+  for (final event in events) {
+    if (event.startTime.isAfter(now)) {
+      return event.startTime;
+    }
+  }
+  return null; // No upcoming events
+}
+
+// Usage example:
+
+
